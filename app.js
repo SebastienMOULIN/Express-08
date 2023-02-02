affc629af6a45bf37fe2317710126e55c6b297c3
sebastienmoulin <sebastien.m.web@gmail.com>
2023-02-02 14:33:09 +0100

    finish

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-require("dotenv").config();
-
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
+const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 const express = require("express");
+
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
-const port = process.env.APP_PORT ?? 5001;
+const port = process.env.APP_PORT ?? 5005;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
-
-const movieHandlers = require("./movieHandlers");
-
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
+app.post("/api/users", hashPassword, userHandlers.postUser);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
+app.get("/api/users", userHandlers.getUsers);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+app.use(verifyToken);
+
+app.delete("/api/movies/:id", verifyToken, movieHandlers.deleteMovie);
 app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
-
-const { hashPassword } = require("./auth.js");
-
-app.post("/api/users", hashPassword, userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
@@ -40,4 +40,17 @@ app.listen(port, (err) => {
   } else {
     console.log(`Server is listening on ${port}`);
   }
+
+  const isItDwight = (req, res) => {
+    if (
+      req.body.email === "dwight@theoffice.com" &&
+      req.body.password === "123456"
+    ) {
+      res.send("Credentials are valid");
+    } else {
+      res.sendStatus(401);
+    }
+  };
+
+  app.post("/api/login", isItDwight);
 });
--- userHandlers.js
+++ userHandlers.js
@@ -37,6 +37,25 @@ const getUsers = (req, res) => {
       res.status(500).send("Error retrieving data from database");
     });
 };
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
 
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
@@ -118,6 +137,7 @@ const deleteUser = (req, res) => {
 };
 
 module.exports = {
+  getUserByEmailWithPasswordAndPassToNext,
   getUsers,
   getUserById,
   postUser,
